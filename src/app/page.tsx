f39bfe09b0a80c71e21adb72e81ee6811737ba22
Ajay Sharma <ajaysharma.13122000@gmail.com>
2025-02-09 08:28:29 +0000

    Add file upload component and update styles in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
+import { FileUpload } from "@/components/ui/file-upload";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col items-center justify-center">
-        <div className="mt-[200px] mb-2">
-          <span className="font-bold text-2xl sm:text-3xl text-pink-800">
-            Made exclusively for Akku
+        <div className="mt-[600px] mb-2">
+          <span className="font-bold text-2xl sm:text-3xl ">
+            Made exclusively for
           </span>
+          <span className="font-bold text-3xl  text-pink-600 ml-2">`Aakanksha`</span>
         </div>
         <div>
-          <span className="text-xl  text-pink-400">
+          <span className="text-xl text-gray-500 ">
             Make any pdf search enabled
           </span>
+        </div>
+        <div className="mt-4 ">
+          <FileUpload />
         </div>
 
 
